ce84556f4d068b1010b30a2df4d7bc0f77b552ce
marjorie <marjorie@pc.com>
2018-03-18 23:31:51 +0100

    wow angry birds

---
+++ Assets/scripts/ProjectileTracking.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTracking : MonoBehaviour {
+
+    public float maxStretch = 3.0f;
+    public LineRenderer catapultLineFront;
+    public LineRenderer catapultLineBack;
+    private SpringJoint2D spring;
+    private Transform catapult;
+    private Ray rayToMouse;
+    private Ray leftCatapultToProjectile;
+    private float maxStretchSqr;
+    private float circleRadius;
+    private bool clickedOn;
+    private Vector2 prevVelocity;
+    private Rigidbody2D rigidbody2d;
+    private CircleCollider2D circle;
+
+    void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+        catapult = spring.connectedBody.transform;
+    }
+
+    void Start()
+    {
+        LineRendererSetup();
+        rayToMouse = new Ray(catapult.position, Vector3.zero);
+        leftCatapultToProjectile = new Ray(catapultLineFront.transform.position, Vector3.zero);
+        maxStretchSqr = maxStretch * maxStretch;
+        circleRadius = circle.radius;
+    }
+
+    void Update()
+    {
+        if (clickedOn)
+            Dragging();
+        if (spring != null)
+        {
+            if (!rigidbody2d.isKinematic && prevVelocity.sqrMagnitude > rigidbody2d.velocity.sqrMagnitude)
+            {
+                Destroy(spring);
+                rigidbody2d.velocity = prevVelocity;
+            }
+            if (!clickedOn)
+                prevVelocity = rigidbody2d.velocity;
+            LineRendererUpdate();
+        }
+        else
+        {
+            catapultLineFront.enabled = false;
+            catapultLineBack.enabled = false;
+        }
+    }
+
+    void LineRendererSetup()
+    {
+        catapultLineFront.SetPosition(0, catapultLineFront.transform.position);
+        catapultLineBack.SetPosition(0, catapultLineBack.transform.position);
+        catapultLineFront.sortingLayerName = "Foreground";
+        catapultLineBack.sortingLayerName = "Foreground";
+        catapultLineFront.sortingOrder = 3;
+        catapultLineBack.sortingOrder = 1;
+    }
+
+    void OnMouseDown()
+    {
+        spring.enabled = false;
+        clickedOn = true;
+    }
+
+    void OnMouseUp()
+    {
+        spring.enabled = true;
+        rigidbody2d.isKinematic = false;
+        clickedOn = false;
+    }
+
+    void Dragging()
+    {
+        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 catapultToMouse = mouseWorldPoint - catapult.position;
+
+        if (catapultToMouse.sqrMagnitude > maxStretchSqr)
+        {
+            rayToMouse.direction = catapultToMouse;
+            mouseWorldPoint = rayToMouse.GetPoint(maxStretch);
+        }
+        mouseWorldPoint.z = 0f;
+        transform.position = mouseWorldPoint;
+    }
+
+    void LineRendererUpdate()
+    {
+        Vector2 catapultToProjectile = transform.position - catapultLineFront.transform.position;
+        leftCatapultToProjectile.direction = catapultToProjectile;
+        Vector3 holdPoint = leftCatapultToProjectile.GetPoint(catapultToProjectile.magnitude + circleRadius);
+        catapultLineFront.SetPosition(1, holdPoint);
+        catapultLineBack.SetPosition(1, holdPoint);
+    }
+}
+
